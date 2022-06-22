d3f56949732e71d6aa8c98c9f0f5be3eeb37f236
wi21b025 <wi21b025@technikum-wien.at>
2022-06-22 14:21:45 +0200

    Initial commit

---
+++ src/main/java/com/example/inperialcal/InperialCalApplication.java
@@ -0,0 +1,13 @@
+package com.example.inperialcal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InperialCalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InperialCalApplication.class, args);
+    }
+
+}
